b618ad1f62ab66d545d89e69a6116c279f7f9e0b
bernard rayoso <rayosobernard@gmail.com>
2023-06-11 12:11:23 +0800

    Add dark and light mode features, add transition on colors when switching mode

--- src/app/components/DarkModeSwitch.jsx
+++ src/app/components/DarkModeSwitch.jsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { MdLightMode } from "react-icons/md";
 import { BsFillMoonFill } from "react-icons/bs";
 import { useTheme } from "next-themes";
@@ -8,20 +8,24 @@ import { useTheme } from "next-themes";
 const DarkModeSwitch = () => {
   const { systemTheme, theme, setTheme } = useTheme();
   const currentTheme = theme === "system" ? systemTheme : theme;
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => setMounted(true), []);
 
   return (
     <>
-      {currentTheme === "dark" ? (
-        <MdLightMode
-          className='hover: cursor-pointer'
-          onClick={() => setTheme("light")}
-        />
-      ) : (
-        <BsFillMoonFill
-          className='hover: cursor-pointer'
-          onClick={() => setTheme("dark")}
-        />
-      )}
+      {mounted &&
+        (currentTheme === "dark" ? (
+          <MdLightMode
+            className='hover: cursor-pointer text-xl hover:text-amber-500 '
+            onClick={() => setTheme("light")}
+          />
+        ) : (
+          <BsFillMoonFill
+            className='hover: cursor-pointer text-xl hover:text-amber-500'
+            onClick={() => setTheme("dark")}
+          />
+        ))}
     </>
   );
 };
