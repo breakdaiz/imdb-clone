6f9d82e6c2f72c45c57c2c343f7de96c29b5b4b5
bernard rayoso <rayosobernard@gmail.com>
2023-06-15 05:27:37 +0800

    create about page

---
+++ src/app/about/page.jsx
@@ -0,0 +1,29 @@
+const About = () => {
+  return (
+    <div className='max-w-6xl mx-auto space-y-4'>
+      <h1 className='text-2xl font-medium text-amber-600'> About</h1>
+      <p>
+        Launched online in 1990 and a subsidiary of Amazon.com since 1998, IMDb
+        is the world's most popular and authoritative source for movie, TV and
+        celebrity content, designed to help fans explore the world of movies and
+        shows and decide what to watch. Our searchable database includes
+        millions of movies, TV and entertainment programs and cast and crew
+        members. IMDb can help you: Jog your memory about a movie, show, or
+        person on the tip of your tongue Find the best movie or show to watch
+        next Empower you to share your entertainment knowledge and opinions with
+        the world’s largest community of fans For fans deciding what to watch
+        and where to watch it, we offer local movie showtimes, ticketing,
+        trailers, critic and user reviews, personalized recommendations, photo
+        galleries, entertainment news, quotes, trivia, box-office data,
+        editorial feature sections and a universal Watchlist. To learn more
+        about watching Trailers, Clips, Featurettes, and IMDb Originals, please
+        see the IMDb Video FAQs. IMDb creates new original video content each
+        week and you can find it all at IMDb Originals. You can find new videos
+        highlighted every day on the IMDb homepage via your desktop computer or
+        IMDb app.
+      </p>
+    </div>
+  );
+};
+
+export default About;
