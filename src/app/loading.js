f0fb744029ad4d8c68aa7c2b2886a9f0b530618a
bernard rayoso <rayosobernard@gmail.com>
2023-06-21 12:18:33 +0800

    update loading classname

--- src/app/loading.js
+++ src/app/loading.js
@@ -2,8 +2,8 @@ import React from "react";
 
 const Loading = () => {
   return (
-    <div className='flex justify-center'>
-      <img src='spinner.svg' alt='loading' />
+    <div className=' flex justify-center'>
+      <img className='h-96' src='spinner.svg' alt='loading...' />
     </div>
   );
 };
