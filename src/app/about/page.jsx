e695e29ac4cee1eb5ba9819caf7a0476cb44d8ec
bernard rayoso <rayosobernard@gmail.com>
2023-06-21 16:16:34 +0800

    fixed error on deployment or running build command

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -4,23 +4,23 @@ const About = () => {
       <h1 className='text-2xl font-medium text-amber-600'> About</h1>
       <p>
         Launched online in 1990 and a subsidiary of Amazon.com since 1998, IMDb
-        is the world's most popular and authoritative source for movie, TV and
-        celebrity content, designed to help fans explore the world of movies and
-        shows and decide what to watch. Our searchable database includes
+        is the world&apos;s most popular and authoritative source for movie, TV
+        and celebrity content, designed to help fans explore the world of movies
+        and shows and decide what to watch. Our searchable database includes
         millions of movies, TV and entertainment programs and cast and crew
-        members.{" "}
+        members.
       </p>
       <p>
         IMDb can help you: Jog your memory about a movie, show, or person on the
         tip of your tongue Find the best movie or show to watch next Empower you
-        to share your entertainment knowledge and opinions with the world’s
+        to share your entertainment knowledge and opinions with the world&apos;s
         largest community of fans For fans deciding what to watch and where to
         watch it, we offer local movie showtimes, ticketing, trailers, critic
         and user reviews, personalized recommendations, photo galleries,
         entertainment news, quotes, trivia, box-office data, editorial feature
         sections and a universal Watchlist. To learn more about watching
         Trailers, Clips, Featurettes, and IMDb Originals, please see the IMDb
-        Video FAQs.{" "}
+        Video FAQs.
       </p>
       <p>
         IMDb creates new original video content each week and you can find it
--- src/app/loading.js
+++ src/app/loading.js
@@ -1,9 +1,10 @@
+import Image from "next/image";
 import React from "react";
 
 const Loading = () => {
   return (
     <div className=' flex justify-center'>
-      <img className='h-96' src='spinner.svg' alt='loading...' />
+      <Image className='h-96' src='spinner.svg' alt='loading...' />
     </div>
   );
 };
